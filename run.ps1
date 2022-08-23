64ea4cdabc4dc4d43819a1945a4b3b95f515d6ef
Mauro <38697944+Alispezzate@users.noreply.github.com>
2022-08-23 18:17:35 +0200

    up

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 # Usage: setenv.ps1 <bot-token> <ngrok url>
 $env:CLESSY_TOKEN = "5763477340:AAFYInOfYsrsHdP64farFJcLyks3_0WuIkw"
-$env:CLESSY_WEBHOOK = $args[0] + "/test"
+$env:CLESSY_WEBHOOK = "https://847b-94-34-24-68.eu.ngrok.io" + "/test"
 $env:CLESSY_DB_DIR = "_data/noto_emojis"
 $env:CLESSY_EMOJI_PATH = "_data"
 $env:CLESSY_UNSPLASH_FONT = "_data/gill.ttf"
